cc1a452fadac45fba8b658f5ef5bf0b40e200053
karim <karim-bahri-18@outlook.fr>
2021-10-10 14:02:35 +0100

    db

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+const { MongoClient } = require("mongodb");
+const DBClient = class {
+  constructor() {
+    const db_host = process.env.DB_HOST || "localhost";
+    const db_port = process.env.DB_PORT || "27017";
+    const db_database = process.env.DB_DATABASE || "files_manager";
+    this.status = false;
+
+    const client = new MongoClient(`mongodb://${db_host}:${db_port}`, {
+      useUnifiedTopology: true,
+    });
+    client.connect((err, data) => {
+      if (!err) {
+        this.status = true;
+        this.db = data.db(db_database);
+      }
+    });
+  }
+  isAlive() {
+    return this.status;
+  }
+  async nbUsers() {
+    return this.db.collection("users").countDocuments();
+  }
+  async nbFiles() {
+    return this.db.collection("files").countDocuments();
+  }
+};
+
+export default new DBClient();
