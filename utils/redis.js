5e43eff203585d3e8875bae77b4a85603c01c8b6
karim <karim-bahri-18@outlook.fr>
2021-10-10 14:02:45 +0100

    redis

---
+++ utils/redis.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import redis from "redis";
+const RedisClient = class {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on("error", (err) => {
+      console.log(err);
+    });
+  }
+  isAlive() {
+    return this.client.connected;
+  }
+  async get(key) {
+    return await this.client.get(key);
+  }
+  async set(key, value, duration) {
+    await this.client.setex(key, value, duration);
+  }
+  async del(key) {
+    await this.client.del(key);
+  }
+};
+export default new RedisClient();
