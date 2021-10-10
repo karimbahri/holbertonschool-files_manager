f3148f0f4e2919ac4b23f47d6209b12cd0b5bb84
karim <karim-bahri-18@outlook.fr>
2021-10-10 14:25:27 +0100

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 
 const route = require("express").Router();
+const AppController = require("../controllers/AppController");
 
 route
   .get("/status", AppController.getStatus)
-  .get("/stats", AppController.getStats);
+  .get("/stats", AppController.getStat);
 
 export default route;
