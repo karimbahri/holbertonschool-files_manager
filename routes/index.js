7c2f8b2017217a717dc120aa7c7eeb4d61695dc0
karim <karim-bahri-18@outlook.fr>
2021-10-11 17:30:30 +0100

    index

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@
 
 const route = require("express").Router();
 const AppController = require("../controllers/AppController");
+const UsersController = require("../controllers/UsersController");
 
 route
   .get("/status", AppController.getStatus)
-  .get("/stats", AppController.getStat);
+  .get("/stats", AppController.getStat)
+  .post("/users", UsersController.postNew);
 
 module.exports = route;
