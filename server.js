7a99928522b52fa40444480c136ffe49832a1f1f
karim <karim-bahri-18@outlook.fr>
2021-10-12 14:49:02 +0100

    req.body middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
-const app = require("express")();
+const express = require("express");
 const route = require("./routes/index");
 
+const app = express();
+app.use(express.json());
 app.use("/", route);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
