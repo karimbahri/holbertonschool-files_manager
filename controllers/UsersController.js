b9eb2811e0fab35df31337919ee5d462d52becdd
karim <karim-bahri-18@outlook.fr>
2021-10-12 14:50:41 +0100

    redis

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,21 +4,21 @@ const DBClient = require("../utils/db");
 const sha1 = require("sha1");
 
 exports.postNew = async (req, res) => {
-  //   try {
-  const { email, password } = req.body;
-  if (!email) return res.status(400).json({ error: "Missing email" });
-  if (!password) return res.status(400).json({ error: "Missing password" });
+  try {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).json({ error: "Missing email" });
+    if (!password) return res.status(400).json({ error: "Missing password" });
 
-  const user = await DBClient.db.collection("users").findOne({ email });
-  if (user) return res.status(400).json({ error: "Already exist" });
+    const user = await DBClient.db.collection("users").findOne({ email });
+    if (user) return res.status(400).json({ error: "Already exist" });
 
-  const h_password = sha1(password);
+    const h_password = sha1(password);
 
-  const user_data = await DBClient.db
-    .collection("users")
-    .insertOne({ email, password: h_password });
-  return res.status(201).send({ email, id: user_data._id });
-  //   } catch (err) {
-  //     return res.status(500).json({ error: err });
-  //   }
+    const user_data = await DBClient.db
+      .collection("users")
+      .insertOne({ email, password: h_password });
+    return res.status(201).send({ email, id: user_data._id });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
 };
